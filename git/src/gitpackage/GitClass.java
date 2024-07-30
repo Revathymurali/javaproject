f66107a569fd704e78786536e8cf0f5ebaa98d39
revathy <revathymurali16@gmail.com>
2024-07-30 10:51:53 +0530

    first commit

---
+++ git/src/gitpackage/GitClass.java
@@ -0,0 +1,12 @@
+package gitpackage;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int A=10;
+int B=20;
+System.out.println(A+B);
+	}
+
+}
